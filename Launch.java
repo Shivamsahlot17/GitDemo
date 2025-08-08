5ce0e4d357b088685ac700c1cb8a8e000ba7c314
Shivamsahlot17 <shivamsahlot6089@gmail.com>
2025-08-08 19:20:28 +0530

    modified file

--- Launch.java
+++ Launch.java
@@ -2,4 +2,10 @@ class Launch{
 	public static void main(){
 		System.out.println("Exploring Git & GitHub");
 	}
+	public static void fun(){
+		int a=10;
+		int b=78;
+		int sum=a+b;
+		System.out.println(sum);
+	}
 }
